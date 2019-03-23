0ca85d890c624ac7bc2032eb778ae2940039875e
Avdey <avdeev.1van@icloud.com>
2019-03-23 11:23:09 +0300

    added design to concert page

--- Front/scripts/concertPageScript.js
+++ Front/scripts/concertPageScript.js
@@ -6,3 +6,15 @@ document.querySelector('.add-poll').addEventListener('click', () => {
     form.style.display = 'block';
   }
 });
+
+document.querySelector('.on-email').addEventListener('click', () => {
+  const ul = document.querySelector('.list-of-concerts');
+  if (ul.style.display === 'flex') {
+    ul.style.display = 'none';
+    document.querySelector('.on-email').style.marginLeft = 'auto';
+  } else {
+    document.querySelector('.on-email').style.marginLeft = '0px';
+    ul.style.marginLeft = 'auto';
+    ul.style.display = 'flex';
+  }
+});
